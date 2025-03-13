4e79711a19b99921f6cab48ce9c756523e2c1c2d
Tomasz Szynkiewicz <tom.szynkiewicz@gmail.com>
2025-03-13 19:35:40 +0100

    chore: basic eslint and prettier setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'perfectionist'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
@@ -21,5 +21,32 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'perfectionist/sort-imports': 'error',
+    'perfectionist/sort-interfaces': [
+      'error',
+      {
+        type: 'natural',
+      },
+    ],
+    'perfectionist/sort-jsx-props': [
+      'error',
+      {
+        type: 'natural',
+      },
+    ],
+    'perfectionist/sort-object-types': [
+      'error',
+      {
+        type: 'natural',
+      },
+    ],
+    'perfectionist/sort-objects': [
+      'error',
+      {
+        type: 'natural',
+      },
+    ],
+    "react/no-unescaped-entities": "off",
+    'sort-keys': 'off',
   },
 };
