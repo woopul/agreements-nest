b1026a4ca1c77358c637ea311b244932da3f5520
Wojtek Opulski <wojtekopulski@gmail.com>
2025-03-17 22:00:03 +0100

    fix: husky pre-commit, adjust eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,14 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
     'perfectionist/sort-imports': 'error',
     'perfectionist/sort-interfaces': [
       'error',
